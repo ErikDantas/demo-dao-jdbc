f9e0282cb9d7976fdd3b9cc1f52bd82a0c0cd0c8
ErikDantas <jose.erikdantas@outlook.com>
2020-07-06 18:32:32 -0300

    Insert new Seller implementation and created the mainapplication navigation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.awt.HeadlessException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.JOptionPane;
@@ -11,25 +15,58 @@ import entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws HeadlessException, ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		SellerDao sellerdao = DaoFactory.createSellerDao();
+		int aux = 0;
+		do {
+			
+			aux = Integer.parseInt(JOptionPane.showInputDialog("Choose: \n"
+					+ "1 - Seller \n"
+					+ "2 - Department \n"
+					+ "3 - Exit \n\n"));
+			if(aux ==1) {
+				int auxseller = Integer.parseInt(JOptionPane.showInputDialog("Choose the option: \n"
+						+ "1 - Find All \n"
+						+ "2 - Find By ID \n"
+						+ "3 - Find By Department \n"
+						+ "4 - Insert a new Seller \n"
+						+ "5 - Update a Seller \n"
+						+ "6 - Delete a Seller by ID \n"));
+				switch(auxseller) {
+					case 1:
+						List<Seller> listseller = sellerdao.findAll();
+						for (Seller obj : listseller) {
+							System.out.println(obj);
+						}
+						break;
+					case 2:
+						Seller seller = sellerdao.findById(Integer.parseInt(JOptionPane.showInputDialog
+								("Insert an ID.\n")));
+						System.out.println(seller);
+						break;
+					case 3:
+						int tempdepid = Integer.parseInt(JOptionPane.showInputDialog("Insert the Department Id: "));
+						Department dep = new Department(tempdepid,null);
+						List<Seller> list = sellerdao.findByDepartment(dep);
+						for (Seller obj : list) {
+							System.out.println(obj);
+						}
+						break;
+					case 4:
+						String name = JOptionPane.showInputDialog("Inser the new seller name: ");
+						String email = JOptionPane.showInputDialog("Inser the new seller email: ");
+						Date birthdate = sdf.parse(JOptionPane.showInputDialog("Inser the new seller birthdate: "));
+						double basesalary = Double.parseDouble(JOptionPane.showInputDialog("Inser the new seller base salary: "));
+						int departmentid = Integer.parseInt(JOptionPane.showInputDialog("Inser the new seller department ID: "));
+						String depname = JOptionPane.showInputDialog("Inser the new seller department name: ");
+						Department depinsert = new Department(departmentid, depname);
+						Seller sellerinsert = new Seller(0, name, email, birthdate, basesalary, depinsert);
+						sellerdao.insert(sellerinsert);
+				}
+			}
+		}while(aux!=3);
 		
-		//Seller seller = sellerdao.findById(Integer.parseInt(JOptionPane.showInputDialog("Qual ID deseja buscar?")));
-		//System.out.println(seller);
-		
-		Department dep = new Department(Integer.parseInt(JOptionPane.showInputDialog("Insira o ID: " )), null);
-		List<Seller> list = sellerdao.findByDepartment(dep);
-		for (Seller obj : list) {
-			System.out.println(obj);
-		}
-		
-		
-		System.out.println("--------------------------------------------");
-		list = sellerdao.findAll();
-		for (Seller obj : list) {
-			System.out.println(obj);
-		}
 
 	}
 
--- src/dao/impl/SellerDaoJDBC.java
+++ src/dao/impl/SellerDaoJDBC.java
@@ -1,6 +1,7 @@
 package dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,7 +27,30 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("insert into seller (Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+"values (?, ?, ?, ?, ?)", st.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new Date(obj.getBirthdate().getTime()));
+			st.setDouble(4, obj.getBasesalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			if(rowsAffected>0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					obj.setId(rs.getInt(1));				
+				}
+				DB.closeResultSet(rs);
+			}else {
+				throw new DbException("Unexpected Failure. No rows affected!");
+			}
+					
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 		
 	}
 
@@ -139,7 +163,8 @@ public class SellerDaoJDBC implements SellerDao {
 				}
 				Seller seller = instantiateSeller(rs, dep);
 				list.add(seller);
-			}
+			}	
+
 			return list;
 		}catch(SQLException e) {
 			throw new DbException(e.getMessage());
